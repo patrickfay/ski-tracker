b30cfc73cd39249dc8a5ebecf7039d623e89d20d
patrickfay <faypa01@gettysburg.edu>
2020-02-17 15:35:00 -0700

    Implemented basic nav-bar

--- app/components/nav-bar/nav-bar.component.js
+++ app/components/nav-bar/nav-bar.component.js
@@ -1,19 +1,17 @@
 angular.module('skiTrackerApp')
   .component('navBar', {
     templateUrl: './components/nav-bar/nav-bar.component.html',
-    // bindings: {
-    //   val: '@',       // value of btn in template
-    //   msg: '@',       // msg to display
-    //   className: '@'  // class of btn in template
-    // },
     controller: 'navBarCtrl'
   })
 
-  .controller('navBarCtrl', function() {
+  .controller('navBarCtrl', function($location) {
     let $ctrl = this;
 
     $ctrl.$onInit = () => {
-      console.log('nav bar here');
+      // init vars here
     };
+
+    // route the user to the specified path using the $location service
+    $ctrl.routeTo = (_path) => $location.url(_path);
 
   });
