50b5b841f1c88d1545f2ec9133d2d1d8511aeeb4
patrickfay <faypa01@gettysburg.edu>
2020-02-27 00:21:54 -0700

    tooltip custom styling

--- app/views/home/home.controller.js
+++ app/views/home/home.controller.js
@@ -2,7 +2,7 @@ angular.module('skiTrackerApp')
   .controller('homeCtrl', function($scope) {
 
     $scope.tooltipMsgs = [
-      'Save as much info about your day of skiing as you can! Any entry you create can have as much or as little information as you want to track! Some of the options are below.',
+      'Save as much info about your day of skiing as you can! Any entry you create can have as much or as little information as you want to track! All listed options are optional.',
       'Filter the days you want to see! Trying to remember a specific day somewhere or with someone? Fastest speed? Apply a filter to find the day you did that!'
     ];
   
