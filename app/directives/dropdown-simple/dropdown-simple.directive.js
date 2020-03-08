840f57033bb98075df60b13fe08b279cb5820e30
patrickfay <faypa01@gettysburg.edu>
2020-03-08 17:55:18 -0600

    updated 'dropdown' to use cust divs and cust styling

--- app/directives/dropdown-simple/dropdown-simple.directive.js
+++ app/directives/dropdown-simple/dropdown-simple.directive.js
@@ -10,11 +10,16 @@ angular.module('skiTrackerApp')
       },
       templateUrl: './directives/dropdown-simple/dropdown-simple.directive.html',
       link: function(scope) {
+        scope.isToggled = false;
+
         // push default value to options array on init
         scope.optionsArr.unshift(!!scope.defaultVal ? scope.defaultVal : '-- select --');
 
         // set model for select to first arr ele
         scope.selectModel = scope.optionsArr[0];
+
+        // toggle dropdown
+        scope.toggleDropdown = () => scope.isToggled = !scope.isToggled;
       }
     };
   });
