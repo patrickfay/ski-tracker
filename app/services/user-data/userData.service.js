8d144f859449c31db8c91b53b65f065702ac0b02
patrickfay <faypa01@gettysburg.edu>
2020-03-17 18:15:55 -0600

    update userData service function comments

--- app/services/user-data/userData.service.js
+++ app/services/user-data/userData.service.js
@@ -2,36 +2,71 @@ angular.module('skiTrackerApp')
   .service('userDataService', function() {
     let $service = this;  // so we don't confuse 'this' w/ scopes across functions and service module
 
-    // we use this variable to hold the user's data across the application
+    // this var is used to hold the user's data across the application
     $service.userData = {
-      entries: null,
+      entries: [],
       skiPartners: []
     };
 
 
-    // add user's entries
+    /**
+     * Set userData.entries to an array of ski entries.
+     * This function sets ALL entries to the parameter passed to this function.
+     * This function should only be called when to add data uploaded by the user.
+     * 
+     * @param {array<entry>} _entries An array of ski day entries.
+     */
     $service.setEntries = (_entries) => {
       $service.userData.entries = _entries;
     };
 
-    // add new ski partner
+    /**
+     * Add a new ski partner to userData.skiPartners.
+     * If the ski partner being added is already in userData.skiPartners, do not add the ski partner.
+     * 
+     * @param {string} _partnerName The name of a ski partner.
+     */
     $service.addSkiPartner = (_partnerName) => {
-      $service.userData.skiPartners.unshift(_partnerName);
+      if (!$service.userData.skiPartners.includes(_partnerName)) {
+        $service.userData.skiPartners.unshift(_partnerName);
+      }
     };
 
-    // get all user's entries
+    /**
+     * Add a new ski entry to userData.entries.
+     * This function adds a NEW ski entry to the user's array fo entries.
+     * This function should only be called when the user has manually entered data for a new entry (likely using the 'new-entry' component)
+     * 
+     * @param {entry} _entry An entry for a ski day.
+     */
+    $service.addEntry = (_entry) => {
+      $service.userData.entries.unshift(_entry);
+    };
+
+    /**
+     * Get all of the user's ski entries.
+     * 
+     * @returns {array<entry>} An array of all the user's ski entries.
+     */
     $service.getEntries = () => {
       return $service.userData.entries;
     };
 
-    // get all user's ski partners
+    /**
+     * Get all of the user's ski partners
+     * 
+     * @returns {array<string>} An array of all the names of the user's ski partners.
+     */
     $service.getSkiPartners = () => {
       return $service.userData.skiPartners;
     };
 
-    // clear data stored in this service
+    /**
+     * Clear all of the user's ski data.
+     * PLEASE ensure to add an alert asking the user if they are sure they would like to do this.
+     */
     $service.clearData = () => {
-      $service.userData.entries = null;
+      $service.userData.entries = [];
       $service.userData.skiPartners = [];
     };
   });
