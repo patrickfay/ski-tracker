399d17b8bb2cd538be0ee0b7e187ec43e671b2ea
patrickfay <faypa01@gettysburg.edu>
2020-03-20 12:56:11 -0600

    first entry modal implemented

--- app/modules/start-fresh/components/first-entry-modal/first-entry-modal.controller.js
+++ app/modules/start-fresh/components/first-entry-modal/first-entry-modal.controller.js
@@ -1,23 +1,56 @@
 angular.module('skiTrackerApp')
   .component('firstEntryModal', {
     bindings: {
-      resolve: '<',
-      close: '&',
-      // dismiss: '&'
+      close: '&'
     },
     templateUrl: './modules/start-fresh/components/first-entry-modal/first-entry-modal.template.html',
     controller: 'firstEntryModalCtrl'
   })
 
-  .controller('firstEntryModalCtrl', function() {
-    console.log('in first entry modal controller yo', 'Yaboo!');
+  .controller('firstEntryModalCtrl', function($scope, userDataService) {
     $ctrl = this;
-    // console.log($ctrl);
-    // console.log($ctrl.resolve);
-    // console.log($ctrl.resolve.items);
 
     $ctrl.$onInit = () => {
-      console.log($ctrl.resolve);
+      $ctrl.timeChanged = false;  // used for ng-class for displayed timer
+      $ctrl.timeLeft = 5;         // time left before redirect, val is displayed in timer
+
+      // start countdown to redirect
+      deductOneSecond();
     };
+
+
+    /**
+     * Deducts one second from timer and adds class to timer div.
+     * If time is not 0, calls function to remove class (that uses transition) and calls itself again.
+     * If no time is left, adds entry obj to user data (which will then display 'entries' view)
+     */
+    function deductOneSecond() {
+      setTimeout(() => {
+        // remove second and add class to timer div
+        --$ctrl.timeLeft;
+        $ctrl.timeChanged = true;
+        $scope.$apply();
+
+        if ($ctrl.timeLeft > 0) {
+          toggleFlash();
+          deductOneSecond();
+        } else {
+          closeModal();
+        }
+      }, 1000);
+    }
+
+    // removes class from timer div
+    function toggleFlash() {
+      setTimeout(() => {
+        $ctrl.timeChanged = false;
+        $scope.$apply();
+      }, 200);
+    }
+
+    // close modal (parent component adds entry on modal close)
+    function closeModal() {
+      setTimeout(() => $ctrl.close(), 300);
+    }
 
   });
